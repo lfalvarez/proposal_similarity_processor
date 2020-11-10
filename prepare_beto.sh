da81f57683ff13064edc1f2365a395ba1f71de22
Felipe Álvarez <luisfelipealvarez@gmail.com>
2020-11-10 18:03:22 -0300

    Agregando cosas de pytorch

---
+++ prepare_beto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://users.dcc.uchile.cl/~jperez/beto/cased_2M/pytorch_weights.tar.gz 
+wget https://users.dcc.uchile.cl/~jperez/beto/cased_2M/vocab.txt 
+wget https://users.dcc.uchile.cl/~jperez/beto/cased_2M/config.json 
+tar -xzvf pytorch_weights.tar.gz
+mv config.json pytorch/.
+mv vocab.txt pytorch/.
