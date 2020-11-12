41c22549e32250de913d0fe136c099ce2a092687
Felipe Álvarez <luisfelipealvarez@gmail.com>
2020-11-12 09:39:19 -0300

    La distancia es euclidiana

--- proposal_similarity_processor/similarity_searcher.py
+++ proposal_similarity_processor/similarity_searcher.py
@@ -3,7 +3,8 @@ from scipy.spatial.distance import cdist
 import numpy as np
 
 class SimilaritySearcher():
-    def __init__(self, search_engine_class: type) -> None:
+    def __init__(self, search_engine_class: type, distance_type: str = 'euclidean') -> None:
+        self.distance_type = distance_type
         self.documents = []
         self.search_engine = search_engine_class()
         self.index_document_id_relations = {}
@@ -16,7 +17,7 @@ class SimilaritySearcher():
 
     def find_nearest(self, value):
         docs = np.asarray(self.documents)
-        return cdist(docs, np.atleast_2d(value)).argmin()
+        return cdist(docs, np.atleast_2d(value), self.distance_type).argmin()
 
     def get_closest(self, text):
         vectorized_input = self.search_engine.vectorize(text)
